561ed77be7d9e979f8b5646dff7d81a32b9f6564
paprawin boonyakida <paprawin@hotmail.com>
2020-10-21 14:51:15 -0400

    lab2 another update

--- packages/lab2/src/run_circle.py
+++ packages/lab2/src/run_circle.py
@@ -9,13 +9,13 @@ class Run_Circle:
 
         # publishes to the output of carm_cmd_switch_node 
         self.pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
-        my_msg = Twist2DStamped() # initialized the message
+        self.my_msg = Twist2DStamped() # initialized the message
     
     # sends the velocity and angular message to the 
     def send_motor_msg(self, vel, angVel):
         self.my_msg.v = vel
         self.my_msg.omega = angVel
-        self.pub.publish(my_msg)
+        self.pub.publish(self.my_msg)
 
 if __name__ == '__main__':
     try:
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         rospy.init_node('run_circle_node', anonymous=True)
         rate = rospy.Rate(1)
         while not rospy.is_shutdown():
-            run_c.send_motor_msg(0.403, 4.0)
+            run_c.send_motor_msg(0.4, 2.3) # edit velocity and omega values here
             rate.sleep()
     except rospy.ROSInterruptException:
             pass
