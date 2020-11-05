e1eff9627cba5d3a4896f9b748f977fd8186e4dd
paprawin boonyakida <paprawin@hotmail.com>
2020-11-05 15:12:22 -0500

    lab3

--- packages/lab3/src/lab3.py
+++ packages/lab3/src/lab3.py
@@ -5,6 +5,9 @@ from std_msgs.msg import Float32, String
 from duckietown_msgs.msg import LanePose, Twist2DStamped, FSMState
 from pid import PID
 
+allow_follow = False
+velocity = 0.23
+
 class Lab3:
     def __init__(self):
 
@@ -12,36 +15,51 @@ class Lab3:
         self.pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
 
         # tune pid values below
-        self.pid_1 = PID(p=1, i=0, d=0)
-        self.pid_2 = PID(p=0, i=0, d=0)
+        self.pid_1 = PID(p=2, i=0, d=0)
+        self.pid_2 = PID(p=0.1, i=0, d=0)
         self.my_msg = Twist2DStamped()
-        self.my_msg.v = 0.23 # velocity
 
         # receive the phi and d to update PID controller and movement
-        rospy.Subscriber("lane_filter_node/lane_pose", LanePose, self.get_new_update)
+        rospy.Subscriber("lane_filter_node/lane_pose", LanePose, self.follow_lane)
 
         # receive command from joystick
         rospy.Subscriber("fsm_node/mode", FSMState, self.callbackFSM)
 
 
     # call PID class control calculation when receiving error message
-    def get_new_update(self, pose):
-        # use first PID controller on d
-        omega_1 = self.pid_1.calc_control(pose.d, 0.001)
+    def follow_lane(self, pose):
+        rospy.logwarn(allow_follow)
 
-        # use second PID controller on phi
-        omega_2 = self.pid_2.calc_control(pose.phi, 0.001)
+        # if joystick key "a" is pressed, run lane follow
+        if (allow_follow):
+            rospy.logwarn("PAPRAWIN LANE FOLLOWING Code")
 
+            self.my_msg.v = velocity
 
-        # add up omega values
-        self.my_msg.omega = omega_1 + omega_2
-        rospy.logwarn("Final Omega: %f", self.my_msg.omega)
+            # use first PID controller on d
+            omega_1 = self.pid_1.calc_control(pose.d, 0.001)
 
-        # sends new controller message to system 
-        self.pub.publish(self.my_msg)
+            # use second PID controller on phi
+            omega_2 = self.pid_2.calc_control(pose.phi, 0.001)
+
+            # add up omega values
+            self.my_msg.omega = omega_1 + omega_2
+            rospy.logwarn("Final Omega: %f", self.my_msg.omega)
+
+            # sends new controller message to system 
+            self.pub.publish(self.my_msg)
+
+        # else will stop running
+        else:
+            self.my_msg.v = 0
+            self.my_msg.omega = 0
+            self.pub.publish(self.my_msg)
+
     def callbackFSM(self, keypressed):
         if (keypressed.state == "LANE_FOLLOWING"):
-            rospy.logwarn("Key pressed: %s", keypressed.state)
+            allow_follow = True # program will run once "a" is pressed
+        elif (keypressed.state == "NORMAL_JOYSTICK_CONTROL"):
+            allow_follow = False # disable lane following program
 
 
 
