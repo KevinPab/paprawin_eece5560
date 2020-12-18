5fe96dea2999d2ae51ae351c5a0b4aa9eaf669d6
paprawin boonyakida <paprawin@hotmail.com>
2020-12-17 22:31:08 -0500

    updated lab5

--- packages/lab5/src/lab5_backup.py
+++ packages/lab5/src/lab5_backup.py
@@ -7,30 +7,38 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import message_filters
 import numpy as np
+from duckietown_msgs.msg import SegmentList
+from sensor_msgs.msg import CompressedImage
 
 #Note: this node utilizes message_filers to synchronize multiple topics to be used in the same callback function
 
-#combines hw8 and hw9 nodes
+#combines hw8 and hw9 class into a class name Lab5
 
-class Homework8:
+class Lab5:
     def __init__(self):
         # Instatiate the converter class once by using a class member
         self.bridge = CvBridge()
-        rospy.Subscriber("image", Image, self.callback_hw8)
-        # publishes to image_cropped topic
-        self.pub_crop = rospy.Publisher("image_cropped", Image, queue_size=10)
-        # publishes to image_white topic
-        self.pub_white = rospy.Publisher("image_white", Image, queue_size=10)
-        # publishes to image_yellow topic
-        self.pub_yellow = rospy.Publisher("image_yellow", Image, queue_size=10)
+        #subscribe to the image from duckiebot's camera
+        rospy.Subscriber("camera_node/image/compressed", CompressedImage, self.callback_lab5, queue_size=1, buff_size=2**24)
+
+        # publishes to line_detector_node/segment_list
+        # the type of message sent in SegmentList
+        self.pub_seglist = rospy.Publisher("line_detector_node/segment_list", SegmentList, queue_size=10)
+
+        self.pub_white = rospy.Publisher("image_lines_white",Image, queue_size=10)
 
     
-    def callback_hw8(self, msg):
+    def callback_lab5(self, msg):
         # convert to a ROS image using the bridge
-        cv_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        cv_img = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
+
+        image_size = (160, 120)
+        offset = 40
+        new_image = cv2.resize(cv_img, image_size, interpolation=cv2.INTER_NEAREST)
+        cv_cropped = new_image[offset:, :]
         
         # crop the image to 50%(vertically). Also note that decimal shape[0] is converted to integer
-        cv_cropped = cv_img[int(cv_img.shape[0]/2):cv_img.shape[0], 0:cv_img.shape[1]]
+        # cv_cropped = cv_img[int(cv_img.shape[0]/2):cv_img.shape[0], 0:cv_img.shape[1]]
 
         # convert colorspace from BGR to HSV
         hsv_img = cv2.cvtColor(cv_cropped, cv2.COLOR_BGR2HSV)
@@ -38,72 +46,45 @@ class Homework8:
         #filter the color from the HSV image
         
         # convert new image to ROS to send
-        ros_cropped = self.bridge.cv2_to_imgmsg(cv_cropped, "bgr8")
+        # ros_cropped = self.bridge.cv2_to_imgmsg(cv_cropped, "bgr8")
 
         
         # publish the cropped image
-        self.pub_crop.publish(ros_cropped)
+        # self.pub_crop.publish(ros_cropped)
 
-        # filter white lane
-        white_filter = cv2.inRange(hsv_img, (0,0,225),(180,40,255))
+        # filter white lane   (0,0,225),(180,40,255)
+        white_filter = cv2.inRange(hsv_img, (0,0,200), (255,50,255))
 
         ros_white = self.bridge.cv2_to_imgmsg(white_filter, "mono8")
 
+        self.pub_white.publish(ros_white)
+
+        # ros_white = self.bridge.cv2_to_imgmsg(white_filter, "mono8")
+
         # publish white filtered image
-        self.pub_white.publish(ros_white) 
+        # self.pub_white.publish(ros_white) 
 
         # filter yellow lane
         yellow_filter = cv2.inRange(hsv_img, (0,150,180),(70,255,255))
 
-        ros_yellow = self.bridge.cv2_to_imgmsg(yellow_filter, "mono8") 
+        # ros_yellow = self.bridge.cv2_to_imgmsg(yellow_filter, "mono8") 
  
         # publish yellow filtered image
-        self.pub_yellow.publish(ros_yellow)     
-
-
-class Homework9:
-    def __init__(self):
-        # Instantiate the converter class once by using a class member
-        self.bridge = CvBridge()
-
-        # subscribes to the cropped img from hw8
-        self.sub_cropped = message_filters.Subscriber("image_cropped", Image)
-
-        # subscribes to image_yellow topic
-        self.sub_yellow = message_filters.Subscriber("image_yellow", Image)
-
-        # subscribes to image_white topic
-        self.sub_white = message_filters.Subscriber("image_white", Image)
-
-        # use time synchronizer to analyze 3 messages in one callback function
-        self.ts = message_filters.TimeSynchronizer([self.sub_cropped, self.sub_yellow, self.sub_white], 10)
-
-        # register the time synchronizer to the callback function
-        self.ts.registerCallback(self.callback)
-
-        # publishes final white filtered image
-        self.pub_white = rospy.Publisher("image_lines_white",Image, queue_size=10)
-
-        # publishes final yellow filtered image
-        self.pub_yellow = rospy.Publisher("image_lines_yellow",Image, queue_size=10)
-
-
-    # this callback function receives 3 msgs with the help of time synchronizer
-    def callback(self, cropped_msg, yellow_msg, white_msg):
+        # self.pub_yellow.publish(ros_yellow)     
 
         # cropped_msg comes in bgr8 format, the rest will be in mono8
         # receive the cropped image
-        cropped_cv = self.bridge.imgmsg_to_cv2(cropped_msg, "bgr8")
+        # cropped_cv = self.bridge.imgmsg_to_cv2(cropped_msg, "bgr8")
 
         # receive the yellow-line image
         # Note: yellow and white lane filtered images come in mono8 format
-        yellow_cv = self.bridge.imgmsg_to_cv2(yellow_msg, "mono8")
+        # yellow_cv = self.bridge.imgmsg_to_cv2(yellow_msg, "mono8")
 
         # receive the white-line image
-        white_cv = self.bridge.imgmsg_to_cv2(white_msg, "mono8")
+        # white_cv = self.bridge.imgmsg_to_cv2(white_msg, "mono8")
 
-        # convert the cropped_image to gray 
-        grey_img = cv2.cvtColor(cropped_cv,cv2.COLOR_BGR2GRAY)
+        # convert the cropped image to gray 
+        grey_img = cv2.cvtColor(cv_cropped,cv2.COLOR_BGR2GRAY)
 
         # perform canny edge detection
         edges = cv2.Canny(grey_img,0,255)
@@ -116,8 +97,8 @@ class Homework9:
         kernel_for_edges = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(1,1))
         
         # dilate white and yellow filtered images
-        dilated_white = cv2.dilate(white_cv, kernel)
-        dilated_yellow = cv2.dilate(yellow_cv, kernel)
+        dilated_white = cv2.dilate(white_filter, kernel)
+        dilated_yellow = cv2.dilate(yellow_filter, kernel)
 
         # dilate the edges to account for lines that are too thin
         dilated_edges = cv2.dilate(edges, kernel_for_edges)
@@ -134,16 +115,26 @@ class Homework9:
         lines_yellow = cv2.HoughLinesP(and_yellow, 1, np.pi/180, 5, None, 2, 2)
 
         # draw blue line on the two images using function provided in homework9 pdf
-        image_lines_white = self.output_lines(cropped_cv, lines_white)
-        image_lines_yellow = self.output_lines(cropped_cv, lines_yellow)
+        #image_lines_white = self.output_lines(cv_cropped, lines_white)
+        #image_lines_yellow = self.output_lines(cv_cropped, lines_yellow)
 
         # convert images back to imgmsg to be published
-        ros_white = self.bridge.cv2_to_imgmsg(image_lines_white, "bgr8")
-        ros_yellow = self.bridge.cv2_to_imgmsg(image_lines_yellow, "bgr8")
 
-        # publish to 2 topics representing white and yellow filtered images
-        self.pub_white.publish(ros_white)
-        self.pub_yellow.publish(ros_yellow)
+        # ros_yellow = self.bridge.cv2_to_imgmsg(image_lines_yellow, "bgr8")
+
+        arr_cutoff = np.array([0, offset, 0, offset])
+        arr_ratio = np.array([1. / cv_cropped[1], 1. / cv_cropped[0], 1. / cv_cropped[1], 1. / cv_cropped[0]])
+        line_normalized_white = (lines_white + arr_cutoff) * arr_ratio
+        line_normalized_yellow = (lines_yellow + arr_cutoff) * arr_ratio
+
+        rospy.logwarn("calculation succeeded \n")
+
+        #create a SegmentList message to be published
+        #SegmentList my_seg_msg;
+
+
+        # publish the message to the topic that ground_projection_node receives
+        #self.pub_seglist(my_seg_msg)
 
     # function to draw blue lines (provided in homework9 instruction)
     def output_lines(self, original_image, lines):
@@ -159,6 +150,5 @@ class Homework9:
 if __name__=="__main__":
     # initialize our node and create a publisher as normal
     rospy.init_node("lab5_node", anonymous=True)
-    hw8 = Homework8()
-    hw9 = Homework9()
+    lab5 = Lab5()
     rospy.spin()
